ade30a32743fada3c42a854b2cc3b0a5067686e8
Meena Alfons <meena.kerolos@gmail.com>
2018-12-06 20:01:14 +0200

    unzip overwrite option

--- scripts/download.sh
+++ scripts/download.sh
@@ -7,14 +7,14 @@ if [ $FILE == "celeba" ]; then
     ZIP_FILE=./data/CelebA_nocrop.zip
     mkdir -p ./data/
     wget -N $URL -O $ZIP_FILE
-    unzip -q $ZIP_FILE -d ./data/
+    unzip -q -o $ZIP_FILE -d ./data/
     rm $ZIP_FILE
 
     # CelebA attribute labels
     URL=https://www.dropbox.com/s/auexdy98c6g7y25/list_attr_celeba.zip?dl=0
     ZIP_FILE=./data/list_attr_celeba.zip
     wget -N $URL -O $ZIP_FILE
-    unzip -q $ZIP_FILE -d ./data/
+    unzip -q -o $ZIP_FILE -d ./data/
     rm $ZIP_FILE
 
 else
