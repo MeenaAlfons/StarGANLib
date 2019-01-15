8ffcb1c945a1a450084b55e917a9fcb998e538fd
Meena Alfons <meena.kerolos@gmail.com>
2019-01-15 23:20:29 +0200

    fix bug in shell script

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,19 +1,20 @@
 FILE=$1
 
 if [ $FILE == "celeba" ]; then
+    __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
     # CelebA images
     URL=https://drive.google.com/file/d/1SSnOlN3Usp9nLYKRyUtRYfYMhiovPOOb/view?usp=sharing
     ZIP_FILE=./data/CelebA_nocrop.zip
     mkdir -p ./data/
-    ./gd.sh $URL $ZIP_FILE
+    bash ${__dir}/gd.sh $URL $ZIP_FILE
     unzip -q -o $ZIP_FILE -d ./data/ | awk 'BEGIN {ORS=" "} {if(NR%10==0)print "."}'
     rm $ZIP_FILE
 
     # CelebA attribute labels
     URL=https://drive.google.com/file/d/1ADCREPUuLKqIB0c1hzriLgu_e0v9rdVX/view?usp=sharing
     ZIP_FILE=./data/list_attr_celeba.zip
-    ./gd.sh $URL $ZIP_FILE
+    bash ${__dir}/gd.sh $URL $ZIP_FILE
     unzip -q -o $ZIP_FILE -d ./data/
     rm $ZIP_FILE
 
